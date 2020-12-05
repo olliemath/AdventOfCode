805705c5d6884aafa3f8f2455af627f4a7d6106e
oliver <oliver@suade.org>
2020-12-05 22:40:13 +0000

    Day 05 - Part 2 (python)

--- python/day_05/solution.py
+++ python/day_05/solution.py
@@ -11,6 +11,17 @@ def parse_boarding_passes(input):
     return [parse_boarding_pass(p) for p in input]
 
 
+def find_missing_seat_ids(passes):
+    existing = set(p[2] for p in passes)
+    return [
+        i for i in range(min(existing), max(existing) + 1)
+        if i not in existing
+    ]
+
+
 def solve(input):
-    highest = max(p[2] for p in parse_boarding_passes(input))
+    passes = parse_boarding_passes(input)
+    highest = max(p[2] for p in passes)
     print("Highest:", highest)
+
+    print("Missing:", find_missing_seat_ids(passes))
--- python/day_05/test_solution.py
+++ python/day_05/test_solution.py
@@ -1,6 +1,8 @@
 import os
 
-from day_05.solution import parse_boarding_pass, parse_boarding_passes
+from day_05.solution import (
+    parse_boarding_pass, parse_boarding_passes, find_missing_seat_ids
+)
 
 
 def test_parse_boarding_pass():
@@ -12,3 +14,10 @@ def test_parse_boarding_passes():
     passes = ["BFFFBBFRRR", "FFFBBBFRRR", "BBFFBBFRLL"]
     expected = [(70, 7, 567), (14, 7, 119), (102, 4, 820)]
     assert parse_boarding_passes(passes) == expected
+
+
+def test_find_missing_seat_ids():
+    input = [(2, 0, 4), (2, 1, 5), (3, 1, 7), (4, 0, 8), (5, 0, 10)]
+    missing = find_missing_seat_ids(input)
+
+    assert missing == [6, 9]
