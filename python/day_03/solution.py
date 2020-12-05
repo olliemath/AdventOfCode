b8fc8832d9a0edb5b0ebf0ada270baa60fe6b08b
olliemath <oliver.margetts@gmail.com>
2020-12-05 00:02:03 +0000

    Day 03 - Part 2

--- python/day_03/solution.py
+++ python/day_03/solution.py
@@ -1,3 +1,12 @@
+PATHS = [
+    (1, 1),
+    (3, 1),
+    (5, 1),
+    (7, 1),
+    (1, 2),
+]
+
+
 def get_grid(lines):
     return [l.strip() for l in lines]
 
@@ -22,5 +31,16 @@ def path_count(grid, x=3, y=1):
     return count
 
 
+def path_product(grid, paths=None):
+    if paths is None:
+        paths = PATHS
+
+    product = 1
+    for x, y in paths:
+        product *= path_count(grid, x=x, y=y)
+
+    return product
+
+
 def solve(input):
-    print(path_count(get_grid(input)))
+    print(path_product(get_grid(input)))
