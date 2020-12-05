a7a49a72f65b85c33f807d08011f1b0c0cdafed3
oliver <oliver@suade.org>
2020-12-05 01:43:18 +0000

    Rust: day 01

---
+++ rust/src/main.rs
@@ -0,0 +1,41 @@
+use std::env;
+use std::fs;
+use std::io::{self, BufRead};
+
+mod day_01;
+
+
+fn load(filename: String) -> Vec<String> {
+    match _load_impl(&filename) {
+        Ok(v) => v,
+        Err(_) => panic!("Failed to open file {}", filename),
+    }
+}
+
+fn _load_impl(filename: &str) -> Result<Vec<String>, io::Error> {
+    let file = fs::File::open(filename)?;
+    let mut result: Vec<String> = Vec::new();
+    for line in io::BufReader::new(file).lines() {
+        match line {
+            Ok(s) => result.push(s),
+            Err(e) => return Err(e),
+        }
+    }
+
+    Ok(result)
+}
+
+
+fn main() {
+
+    let args: Vec<String> = env::args().collect();
+    assert_eq!(args.len(), 3, "Wrong number of args. Usage:\n    runner day input_dir\n");
+
+    let day = args[1].parse::<i32>().expect("day should be an integer");
+    let input = format!("{}/day_{:02}.txt", args[2], day);
+
+    match day {
+        1 => day_01::solve(load(input)),
+        _ => panic!("No solution for {} provided", day),
+    }
+}
