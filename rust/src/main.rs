e601670b4d08459fa6996dc70a3b66e30c841527
olliemath <oliver.margetts@gmail.com>
2020-12-06 18:46:16 +0000

    Day 06 (rust)

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -4,6 +4,7 @@ use std::io::{self, BufRead};
 
 mod day_01;
 mod day_05;
+mod day_06;
 
 
 fn load(filename: String) -> Vec<String> {
@@ -38,6 +39,7 @@ fn main() {
     match day {
         1 => day_01::solve(&load(input)),
         5 => day_05::solve(&load(input)),
+        6 => day_06::solve(&load(input)),
         _ => panic!("No solution for {} provided", day),
     }
 }
